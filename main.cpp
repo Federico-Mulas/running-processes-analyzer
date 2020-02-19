14acb5b758e5d6336e4c8b7b74eeac2a984cfcc5
Federico Mulas <federico.mulas@alphacogs.com>
2020-02-19 23:21:10 +0100

    final version

--- main.cpp
+++ main.cpp
@@ -1,49 +1,98 @@
-#include <iostream>
 #include "./parser.hpp"
-
+#include "./utils.hpp"
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Exception.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/cURLpp.hpp>
+#include <iostream>
+#include <mutex>
 #include <nlohmann/json.hpp>
+#include <sstream>
+#include <thread>
+#include <vector>
+
 using json = nlohmann::json;
 using namespace std;
+using namespace rpa;
 
+// static variables
+static const char *config_url =
+    "http://my-json-server.typicode.com/skatsev/testproject/posts/2";
+static constexpr chrono::seconds probe_frequency = 5s;
+static constexpr chrono::seconds config_probe_frequency = 3s;
 
-#include <curlpp/cURLpp.hpp>
-#include <curlpp/Easy.hpp>
-#include <curlpp/Options.hpp>
-#include <curlpp/Exception.hpp>
-#include <sstream>
+void utilizationProber(atomic_flag &app_list_lock,
+                       const vector<string> &app_list, mutex &pause_mutex,
+                       condition_variable &pause_condition) {
+  ps_parser parser;
+  try {
+    while (true) {
+      while (!app_list_lock.test_and_set(memory_order_acquire)) // spinlock
+        ;
+      if (app_list.size() > 0) {
+        parser.setSource(popen("ps aux", "r"));
+        while (parser) {
+          info i = parser.line();
+          for (const string &command : app_list) {
+            if (i.command == command)
+              std::cout << i.command << " " << i.pid << " " << i.cpu << " "
+                        << i.mem << std::endl;
+          }
+        }
+        app_list_lock.clear(memory_order_release); // end critical section
+        this_thread::sleep_for(probe_frequency);
+      } else {
+        app_list_lock.clear(memory_order_release); // end critical section
+        unique_lock<mutex> lock(pause_mutex);
+        pause_condition.wait(lock);
+      }
+    }
+  } catch (std::system_error &err) {
+    app_list_lock.clear(); // ensure clear of the lock
+    cerr << err.what();
+  }
+}
 
 int main() {
-    ps_parser parser;
-    if(!parser)
-        return 1;
-    /*
-    do {
-        info i = parser.line();
-        std::cout << i.command << " " << i.pid << " " << i.cpu << " " << i.mem << std::endl;
-    } while(parser);
-    */
-    
-    json j;
-    j["pi"] = 3.141;
-    cout << j << endl;
-    
-    	try 
-	{
-		curlpp::Cleanup cleaner;
-        ostringstream s;
-		s << curlpp::options::Url("http://my-json-server.typicode.com/skatsev/testproject/posts/1");
-        j = json::parse(s.str());
-        cout << j << endl;
+  mutex pause_mutex;
+  condition_variable pause_condition;
+  atomic_flag app_list_lock(false);
+  vector<string> app_list;
+  thread prober(utilizationProber, ref(app_list_lock), ref(app_list),
+                ref(pause_mutex), ref(pause_condition));
 
-	}
-
-	catch ( curlpp::LogicError & e ) 
-	{
-		std::cout << e.what() << std::endl;
-	}
-
-	catch ( curlpp::RuntimeError & e )
-	{
-		std::cout << e.what() << std::endl;
-	}
+  json j;
+  while (true) {
+    try {
+      curlpp::Cleanup cleaner;
+      ostringstream s;
+      s << curlpp::options::Url(config_url);
+      j = json::parse(s.str());
+      // conversion to avoid locks
+      vector<string> new_app_list = j["applications"].get<vector<string>>();
+      for (string &i : new_app_list) {
+        rpa::to_lower(i);
+        cout << i;
+      }
+      cout << endl;
+      size_t n_apps = app_list.size();
+      if (!(n_apps == 0 && new_app_list.size() == 0)) {
+        while (!app_list_lock.test_and_set(memory_order_acquire)) // spinlock
+          ;
+        app_list = new_app_list;
+        app_list_lock.clear(memory_order_release); // end critical section
+        if (n_apps == 0) {
+          pause_condition.notify_one();
+        }
+        this_thread::sleep_for(config_probe_frequency);
+      }
+    } catch (curlpp::LogicError &e) {
+      cerr << e.what() << endl;
+    } catch (curlpp::RuntimeError &e) {
+      cerr << e.what() << endl;
+    }
+  }
 }
--- parser.hpp
+++ parser.hpp
@@ -1,115 +1,106 @@
-#ifndef  CATCH_TEST_PARSER_HPP
+#ifndef CATCH_TEST_PARSER_HPP
 #define CATCH_TEST_PARSER_HPP
-#include <string>
 #include <cstring>
-#include <stdio.h>
-#include <system_error>
 #include <functional>
+#include <stdio.h>
+#include <string>
+#include <system_error>
+
+namespace rpa {
 
 struct info {
-    unsigned int pid;
-    float cpu;
-    float mem;
-    std::string command;
+  unsigned int pid;
+  float cpu;
+  float mem;
+  std::string command;
 };
 
 class ps_parser {
-    enum positions {
-        pid,
-        cpu,
-        mem,
-        command,
-        max
-    };
-    
-    FILE* out;
-    static const size_t buffer_size = 1024;
-    int positions[max];
-    static char const* monitored[max];
+  enum positions { pid, cpu, mem, command, max };
+
+  FILE *out;
+  static const size_t buffer_size = 1024;
+  int positions[max];
+  static char const *monitored[max];
+
 public:
-    
-    
-    ps_parser() {
-        out = popen("ps aux", "r");
-        if(out == nullptr || ferror(out))
-            throw std::system_error(std::error_code(ferror(out), std::system_category()), "error opening ps");
-        //get first line to get positions
-        line([this](char* token, int index){
-            for(int i = 0; i < max; ++i) {
-                if(std::strcmp(token, ps_parser::monitored[i]) == 0) 
-                    this->positions[i] = index;
-            }
-        });
-    }
-    
-    info line() {
-        info res;
-        line([this, &res](char* token, int index){
-            if(index == this->positions[pid]) {
-                res.pid = std::stoi(token);
-                return;
-            }
-            if(index == this->positions[cpu]) {
-                res.cpu = std::stof(token);
-                return;
-            }
-            
-            if(index == this->positions[mem]) {
-                res.mem = std::stof(token);
-                return;
-            }
-            
-            if(index == this->positions[command]) {
-                /*
-                int pos = 0;
-                for(int i = 0; token[i]; i++) {
-                    if(token[i] == '/')
-                        pos = i + 1;
-                }
-                res.command = (token + pos);
-                */
-                res.command = token;
-                return;
-            }
-        });
-        return res;
-    }
-    
-    explicit operator bool() {
-        return feof(out) == 0;
-    }
-    
-    ~ps_parser() {
-        pclose(out);
-    }
-private:
-    void line(const std::function<void(char*, int)> &fun) {
-        char buff[buffer_size]; //a token should never exceed 255 (path length)
-        int i = 0;
-        int token_n = 0;
-        for(int c = fgetc(out); c != EOF && c!= '\n' ; c = fgetc(out)) {
-            if(c == ' ') {
-                if(i != 0) {
-                    buff[i] = 0;
-                    fun(buff, token_n);
-                    token_n++;
-                    i = 0;
-                }
-                continue;
-            }
-            buff[i] = c;
-            i++;
+  ps_parser() {}
+
+  void setSource(FILE *file) {
+    out = file;
+    if (out == nullptr || ferror(out))
+      throw std::system_error(
+          std::error_code(ferror(out), std::system_category()),
+          "error opening ps");
+    // get first line to get positions
+    line([this](char *token, int index) {
+      for (int i = 0; i < max; ++i) {
+        if (std::strcmp(token, ps_parser::monitored[i]) == 0)
+          this->positions[i] = index;
+      }
+    });
+  }
+
+  info line() {
+    info res;
+    line([this, &res](char *token, int index) {
+      if (index == this->positions[pid]) {
+        res.pid = std::stoi(token);
+        return;
+      }
+      if (index == this->positions[cpu]) {
+        res.cpu = std::stof(token);
+        return;
+      }
+
+      if (index == this->positions[mem]) {
+        res.mem = std::stof(token);
+        return;
+      }
+
+      if (index == this->positions[command]) {
+        /*
+        int pos = 0;
+        for(int i = 0; token[i]; i++) {
+            if(token[i] == '/')
+                pos = i + 1;
         }
-        buff[i] = 0;
-        fun(buff, token_n);
+        res.command = (token + pos);
+        */
+        res.command = token;
+        return;
+      }
+    });
+    return res;
+  }
+
+  explicit operator bool() { return feof(out) == 0; }
+
+  ~ps_parser() { pclose(out); }
+
+private:
+  void line(const std::function<void(char *, int)> &fun) {
+    char buff[buffer_size]; // a token should never exceed 255 (path length)
+    int i = 0;
+    int token_n = 0;
+    for (int c = fgetc(out); c != EOF && c != '\n'; c = fgetc(out)) {
+      if (c == ' ') {
+        if (i != 0) {
+          buff[i] = 0;
+          fun(buff, token_n);
+          token_n++;
+          i = 0;
+        }
+        continue;
+      }
+      buff[i] = c;
+      i++;
     }
+    buff[i] = 0;
+    fun(buff, token_n);
+  }
 };
 
-char const* ps_parser::monitored[max] = {
-    "PID",
-    "%CPU",
-    "%MEM",
-    "COMMAND"
-};
-
+char const *ps_parser::monitored[max] = {"PID", "%CPU", "%MEM", "COMMAND"};
+} // namespace rpa
 #endif
